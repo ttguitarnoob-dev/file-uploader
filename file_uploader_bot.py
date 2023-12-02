221b0d56e4d7e706d2ac5a965f17fd43115eb3ee
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-12-02 11:08:33 -0600

    corrected the path that I got wrong like a dummy

--- file_uploader_bot.py
+++ file_uploader_bot.py
@@ -3,13 +3,17 @@ import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = '/home/fireweed-guitars/build/static/media'
+UPLOAD_FOLDER = '/home/travis/frontend/fireweed-guitars/build/static/media'
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def home():
+    return "Hello home"
 
 @app.route('/photos', methods=['POST'])
 def photo_upload():
